65ad17a3c73634fd3009b5d7813c126c56a9a21a
alexsinfarosa <as898@cornell.edu>
2016-11-28 16:29:16 -0500

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -43,6 +44,21 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => {
+    res.status(404).send(e);
+  })
 });
 
 app.listen(port, () => {
